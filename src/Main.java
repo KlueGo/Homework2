877b7170f27428821a0ac944afeca4818a8135ee
KlueGo <klimatop777@gmail.com>
2025-05-29 23:42:05 +0300

    Loading...

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    var dog = 8.0;
+    var cat = 3.6;
+    var paper = 763789;
+    var x = 19;
+    var y = x + 2;
+    var z = y / 7;
+    var friend = (x + ",") + (y + ",") + (z + ",");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+
+
+    System.out.println(dog + 4 - 3.5);
+    System.out.println(cat + 4 - 1.6);
+    System.out.println(paper + 4 - 7639);
+    System.out.println(friend);
+
+}
 }
