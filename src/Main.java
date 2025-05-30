b6ebb3e158bc1083ed3d14439d05b9361e646845
KlueGo <klimatop777@gmail.com>
2025-05-30 17:31:05 +0300

    Loading ...

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,46 @@
 public class Main {
     public static void main(String[] args) {
+
+        //Task 1,2,3
+
     var dog = 8.0;
     var cat = 3.6;
     var paper = 763789;
 
-    var x = 19;
-    var y = x + 2;
-    var z = y / 7;
-    var friend = (x + ",") + (y + ",") + (z + ",");
+    dog += 4;
+    cat += 4;
+    paper += 4;
 
-        var o = 3.5;
-        var l = o * 10;
-        var m = l  / 3.5;
-        var n = m + 4;
-        var frog = (o + ",") + (l + ",") + (m + ",") + (n + ",");
+    dog -= 3.5;
+    cat -= 1.6;
+    paper -= 7639;
+
+    System.out.println(dog + "," + cat + "," + paper + ",");
+
+
+        //Task4
+
+    var friend = 19;
+        System.out.println(friend);
+    friend += 2;
+        System.out.println(friend);
+    friend /=7;
+        System.out.println(friend);
+
+
+        //Task5
+
+    var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
+        frog += 4;
+        System.out.println(frog);
+
+
+        //Task6,7
 
         var boxMas1 = 78.2;
         var boxMas2 = 82.7;
@@ -21,27 +48,15 @@ public class Main {
         var difMass = boxMas2 - boxMas1;
         var masRem = boxMas2 % boxMas1;
 
+        System.out.println((sumMass + ",") + (difMass + ",") + (masRem + ",") );
+
+        //Task8
+
         var allTime = 640;
         var workTime = 8;
         var workers = allTime / workTime;
         var workers2 = workers + 94;
         var workTime2 = allTime / workers2;
-
-
-
-
-
-        System.out.println(dog + 4 - 3.5);
-
-        System.out.println(cat + 4 - 1.6);
-
-        System.out.println(paper + 4 - 7639);
-
-        System.out.println(friend);
-
-        System.out.println(frog);
-
-        System.out.println((sumMass + ",") + (difMass + ",") + (masRem + ",") );
 
         System.out.println("Всего работников в компании - " + workers + " человек.");
 
